2b776028fb297345d20ff2afc602977af4aefb42
bader1263 <38397019+bader1263@users.noreply.github.com>
2018-04-15 18:38:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require ('discord.js');
 
 var bot = new Discord.Client();
 var prefix = ('.');
+var randnum = 0;
 
 bot.on('ready', () => {
     bot.user.setPresence({ game: {name: '.help Baderbot', type: 0}});
@@ -20,23 +21,37 @@ bot.on('message',message => {
     if (message.content === prefix +'help'){
         var help_embed = new Discord.RichEmbed()
         .setColor('#25E6B9')
-        .addField("Commande du bot!","   -.help : Affiche cette page (Génius !)","   -.invitation: Donne le lien d'invitation vers le discord bader comunity et le lien pour ajouter le bot )
-        .addField("Interaction","ping: pong ahaha c'est tres drole.")
+        .addField("Commande du bot!","   -.help : Affiche cette page (Génius !)","   -.invitation: Donne le lien d'invitation vers le discord bader comunity et le lien pour ajouter le bot ")
+        .addField("Interaction","ping: pong ahaha c'est tres drole.","Bonjour: Le bot repond bonjour (ou pas)")
         .setFooter("Page de help")
         message.channel.sendEmbed(help_embed);
-    //message.channel.sendMessage("Commande du bot:\n .help: Affiche cette page (Genius)\n ping:pong !");
     console.log("La comande de help a été demander");
     }
-
     if (message.content === prefix +'invitation'){
-        var invitation_embed = new Discord.RichEmbed()
+         var invitation_embed = new Discord.RichEmbed()
         .setColor('#25E6B9')
         .addField("invitation du bot","   Lien du bot : https://discordapp.com/oauth2/authorize?client_id=432162437681381380&scope=bot&permissions=2146958591")
         .addField("invitation du serveur bader comunity","Lien d'invitation: https://discord.gg/Nm8SXgm")
         .setFooter("Page d'invitation")
         message.channel.sendEmbed(invitation_embed);
-    //message.channel.sendMessage("Commande du bot:\n .help: Affiche cette page (Genius)\n ping:pong !");
     console.log("La comande d'invitation a été demander");
     }
+    if (message.content === "Bonjour"){
+        if (randnum == 1){
+            message.reply ("Bonjour , comment vas tu ?");
+            console.log("Le bot a été poli");
+            
+        }
 
+        if (randnum == 2){
+            message.reply("Aurevoire");
+            console.log("le bot n'a pas été poli !")
+        }
+
+    }
 });
+function random(min, max) {
+    min = Math.cell(0);
+    max = Math.floor(3);
+   randnum = Math.floor(Math.random() * (max - min +1) + min);
+}
